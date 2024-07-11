4c26ae1af028754759ac77ea7ffea929e68b896f
Rybka <nikitarybalkin1@gmail.com>
2024-07-11 18:09:49 +0500

    Changes to be committed: 	deleted:    app/_layout.jsx 	modified:   app/index.jsx 	deleted:    assets/images/adaptive-icon.png 	deleted:    assets/images/favicon.png 	new file:   assets/images/home.png 	deleted:    assets/images/icon.png 	new file:   assets/images/menu.png 	new file:   assets/images/message.png 	new file:   assets/images/qr.png 	deleted:    assets/images/splash.png 	deleted:    components/Navbar.jsx 	new file:   constants/icons.jsx 	modified:   package-lock.json 	modified:   package.json

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
 
+
+import icons from '../constants/icons';
+
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Image } from 'react-native';
 
@@ -13,22 +16,22 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
    
-      <Tab.Navigator>
+    <Tab.Navigator 
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            const iconName = focused ? icons[route.name].active : icons[route.name].inactive;
+            return <Image source={iconName} style={{ width: size, height: size, tintColor: color }} />;
+          },
+          tabBarActiveTintColor: 'red',
+          tabBarInactiveTintColor: 'black',
+        })}
+    >
        
-        <Tab.Screen name="Home" component={HomeScreen} screenOptions={{   tabBarIcon: ({ focused, color, size }) => (
-              <Image
-                source={
-                  focused
-                    ? require('../assets/images/icon.png')
-                    : require('../assets/images/icon.png')
-                }
-                style={{ width: 24, height: 24, tintColor: color }}
-              />
-            ),}} 
-        />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-        <Tab.Screen name="Notifications" component={NotificationsScreen} />
+        <Tab.Screen name="Home" component={HomeScreen} options={{ tabBarLabel: 'Главная' }} /> 
+        
+        <Tab.Screen name="Qr" component={SettingsScreen} options={{ tabBarLabel: 'App-Qr' }} />
+        <Tab.Screen name="Notifications" component={ProfileScreen} options={{ tabBarLabel: 'Сообщения' }} />
+        <Tab.Screen name="Servise" component={NotificationsScreen} options={{ tabBarLabel: 'Сервисы' }} />
       </Tab.Navigator>
    
   );
---
+++ constants/icons.jsx
@@ -0,0 +1,22 @@
+ 
+const icons = {
+    Home: {
+        active: require('../assets/images/home.png'),
+        inactive: require('../assets/images/home.png'),
+    },
+    Qr: {
+        active: require('../assets/images/qr.png'),
+        inactive: require('../assets/images/qr.png'),
+    },
+    Servise: {
+        active: require('../assets/images/menu.png'),
+        inactive: require('../assets/images/menu.png'),
+    },
+    Notifications: {
+        active: require('../assets/images/message.png'),
+        inactive: require('../assets/images/message.png'),
+    },
+};
+
+export default icons;
+  
