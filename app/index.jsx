68ecfd7c75fd9dbc80acc0059bc362c9b84aee0f
Rybka <nikitarybalkin1@gmail.com>
2024-07-11 12:19:44 +0500

    Changes to be committed:  	modified:   .gitignore  	deleted:    app/(tabs)/_layout.tsx  	deleted:    app/(tabs)/explore.tsx  	deleted:    app/(tabs)/index.tsx  	deleted:    app/+html.tsx  	deleted:    app/+not-found.tsx  	new file:   app/_layout.jsx  	deleted:    app/_layout.tsx  	new file:   app/index.jsx  	deleted:    components/Collapsible.tsx  	deleted:    components/ExternalLink.tsx  	deleted:    components/HelloWave.tsx  	deleted:    components/ParallaxScrollView.tsx  	deleted:    components/ThemedText.tsx  	deleted:    components/ThemedView.tsx  	deleted:    components/__tests__/ThemedText-test.tsx  	deleted:    components/__tests__/__snapshots__/ThemedText-test.tsx.snap  	deleted:    components/navigation/TabBarIcon.tsx  	modified:   package-lock.json  	modified:   package.json  	new file:   src/input.css  	new file:   src/output.css  	new file:   tailwind.config.js  	modified:   tsconfig.json
      Changes not staged for commit:
     	modified:   app/_layout.jsx
     	modified:   app/index.jsx
     	modified:   babel.config.js
     	deleted:    constants/Colors.ts
     	deleted:    hooks/useColorScheme.ts
     	deleted:    hooks/useColorScheme.web.ts
     	deleted:    hooks/useThemeColor.ts
     	modified:   package-lock.json
     	modified:   package.json
     	deleted:    src/input.css
     	deleted:    src/output.css
     	modified:   tailwind.config.js
     	deleted:    tsconfig.json
    
      Untracked files:
     	components/
     	styles.js

---
+++ app/index.jsx
@@ -0,0 +1,18 @@
+import {View, Text} from 'react-native';
+import '../src/input.css';
+
+
+const Home =() => {
+
+    return (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>Home Screen</Text>
+            <div className='mt-12 bg-slate-900 text-4xl'>
+                asda
+            </div>
+        </View>
+    );
+
+}
+
+export default Home;
