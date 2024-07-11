7af109a97099ea63a44133674da932d3625aaa0b
Rybka <nikitarybalkin1@gmail.com>
2024-07-11 13:14:15 +0500

    Changes to be committed: 	modified:   app/index.jsx 	new file:   app/pages/HomeScreen.jsx 	new file:   app/pages/NotificationsScreen.jsx 	new file:   app/pages/ProfileScreen.jsx 	new file:   app/pages/SettingsScreen.jsx 	deleted:    assets/images/partial-react-logo.png 	deleted:    assets/images/react-logo.png 	deleted:    assets/images/react-logo@2x.png 	deleted:    assets/images/react-logo@3x.png 	modified:   package-lock.json 	modified:   package.json

--- app/index.jsx
+++ app/index.jsx
@@ -1,21 +1,35 @@
-import {View, Text} from 'react-native';
+import * as React from 'react';
 
- 
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Image } from 'react-native';
 
-import  Navbar  from "../components/Navbar"
+import HomeScreen from './pages/HomeScreen';
+import SettingsScreen from './pages/SettingsScreen';
+import ProfileScreen from './pages/ProfileScreen';
+import NotificationsScreen from './pages/NotificationsScreen';
 
+const Tab = createBottomTabNavigator();
 
-const Home =() => {
-
-    return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>Home Screen</Text>
-
-                <Navbar/>
-             
-        </View>
-    );
-
+export default function App() {
+  return (
+   
+      <Tab.Navigator>
+       
+        <Tab.Screen name="Home" component={HomeScreen} screenOptions={{   tabBarIcon: ({ focused, color, size }) => (
+              <Image
+                source={
+                  focused
+                    ? require('../assets/images/icon.png')
+                    : require('../assets/images/icon.png')
+                }
+                style={{ width: 24, height: 24, tintColor: color }}
+              />
+            ),}} 
+        />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Screen name="Notifications" component={NotificationsScreen} />
+      </Tab.Navigator>
+   
+  );
 }
-
-export default Home;
